7094fa4e2f86390a2a7e5cd8417f1dc43c5292bf
Act-LuisArellano <arellano.luis.3c@gmail.com>
2021-10-03 03:22:52 -0500

    port update

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ require('./database');
 require('./config/passport');
 
 // Settings
-app.set('port',process.env.PORT || 3000);
+app.set('port',process.env.PORT || 8080);
 app.set('views',path.join(__dirname,'views'));
 const hbs = exphbs.create({
     defaultLayout: 'main',
